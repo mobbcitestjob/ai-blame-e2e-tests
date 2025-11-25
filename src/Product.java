43aa71760c8bbfcf8151c5210616d259265bb728
Lior Or <lior@mobb.ai>
2025-11-25 23:34:08 +0100

    Add header to User, remove constructor from Product

--- src/Product.java
+++ src/Product.java
@@ -1,8 +1,3 @@
-public class Product {
-  private String id;
-  private String name;
-  private double price;
-
   public Product(String id, String name, double price) {
     this.id = id;
     this.name = name;
