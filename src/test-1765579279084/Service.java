2677fbf1f31e9bb3c8a7ea9de4091a6507241ee1
Lior Or <lior@mobb.ai>
2025-12-12 23:41:22 +0100

    Add Product, Service, Helper, and Config classes

---
+++ src/test-1765579279084/Service.java
@@ -0,0 +1,15 @@
+public class Service {
+  private String serviceName;
+
+  public Service(String serviceName) {
+    this.serviceName = serviceName;
+  }
+
+  public String getServiceName() {
+    return serviceName;
+  }
+
+  public void execute() {
+    System.out.println("Executing: " + serviceName);
+  }
+}
