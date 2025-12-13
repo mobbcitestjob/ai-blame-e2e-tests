ccf9ecd701138261875ce33553d2cc064e31208f
Lior Or <lior@mobb.ai>
2025-12-13 16:50:34 +0100

    Delete Helper and extend Config with debug mode

--- src/test-1765641027957/Config.java
+++ src/test-1765641027957/Config.java
@@ -1,11 +1,21 @@
 public class Config {
   private String environment;
+  private boolean debugMode;
 
   public Config(String environment) {
     this.environment = environment;
+    this.debugMode = false;
   }
 
   public String getEnvironment() {
     return environment;
+  }
+
+  public void enableDebug() {
+    this.debugMode = true;
+  }
+
+  public boolean isDebugMode() {
+    return debugMode;
   }
 }
